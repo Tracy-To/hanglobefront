65325146d5157de1e4bc6a1d713b705a19b05363
Tracy To <tracyto101@gmail.com>
2024-07-02 23:26:09 -0400

    made ArticleDetails.js from ArticleCard.js

--- src/components/ArticleDetails.js
+++ src/components/ArticleDetails.js
@@ -1,7 +1,59 @@
 import React from 'react'
 
-const ArticleDetails = (props) => {
-  return <h1>Article Detail</h1>
+// the same as ArticleCard.js but this does not splice the content
+
+// link each backend name to its frontend name 
+const categoryNames = {
+  sights_landmarks: 'Sights & Landmarks',
+  cities_towns: 'Cities & Towns',
+  food_dining: 'Food & Dining',
+  cafes: 'Cafés',
+  activities_leisure: 'Activities & Leisure',
+  daily_life: 'Daily Life Essentials',
+  travel_tips: 'Travel Tips'
+}
+
+const ArticleDetails = ({article}) => {
+  // get the frontend name based on the backend name
+  const categoryName = categoryNames[article.category] || article.category
+
+  // format the 'created_at' and 'updated_at' dates
+  const formattedCreatedAt = new Date(article.created_at).toLocaleDateString('en-US', {
+    // ex. 2024
+    year: 'numeric',
+    // ex. July
+    month: 'long',
+    // ex. 2
+    day: 'numeric'
+  })
+
+  const formattedUpdatedAt = new Date(article.updated_at).toLocaleDateString('en-US', {
+    // ex. 2024
+    year: 'numeric',
+    // ex. July
+    month: 'long',
+    // ex. 2
+    day: 'numeric'
+  })
+
+  // create Date objects without microseconds for comparison
+  const createdAt = new Date(article.created_at).setMilliseconds(0)
+  const updatedAt = new Date(article.updated_at).setMilliseconds(0)
+
+  // check if article has been updated
+  const hasBeenUpdated = createdAt !== updatedAt
+
+  return (
+    <div>
+      <h2>{article.title}</h2>
+      <p>Category: {categoryName}</p>
+      <p>{article.content}</p>
+      {article.media && <img src={article.media} alt="" />}
+      {article.author && <p>Written by: {article.author}</p>}
+      <p>Written on: {formattedCreatedAt}</p>
+      {hasBeenUpdated && <p>Last updated on: {formattedUpdatedAt}</p>}
+    </div>
+  )
 }
 
 export default ArticleDetails
