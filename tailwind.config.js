d1489b0789489bd8c4642f6c453f59e7345c0850
Tracy To <tracyto101@gmail.com>
2024-07-08 17:22:08 -0400

    removed theme colors not used

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        peach: {
-          100: '#FFE5D4',
-          200: '#FFD1B8',
-          300: '#FFBE9C',
-          400: '#FFAA80',
-          500: '#FF9664',
-      },
     },
   },
   plugins: [],
